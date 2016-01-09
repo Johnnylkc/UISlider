8661592e590d1febf24556614d46553363beacb7
Johnnylkc <momozzz1231@gmail.com>
2016-01-09 17:05:28 +0800

    UISlider

--- UISlider/ViewController.swift
+++ UISlider/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var slider = UISlider()
+    var resultLabel = UILabel()
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+        slider.frame = CGRectMake(0, 0, 300, 30)
+        slider.center = CGPointMake(self.view.frame.size.width/2, 100)
+        slider.minimumValue = 1
+        slider.maximumValue = 100
+        slider.addTarget(self, action: "changeLabelText:", forControlEvents: .TouchDragInside)
+        self.view.addSubview(slider)
+        
+        resultLabel.frame = CGRectMake(0, 0, 50, 30)
+        resultLabel.center = CGPointMake(self.view.frame.size.width/2, 150)
+        resultLabel.textAlignment = .Center
+        resultLabel.font = UIFont.boldSystemFontOfSize(30)
+        resultLabel.textColor = UIColor.orangeColor()
+        resultLabel.text = "0"
+        self.view.addSubview(resultLabel)
+    
+    
+    }
+    
+    func changeLabelText(sender:UISlider)
+    {
+        let finalValue = Int(sender.value)
+        resultLabel.text = "\(finalValue)"
     }
 
     override func didReceiveMemoryWarning() {
