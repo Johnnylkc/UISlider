7a882d74efef9355464c240557c9b0f11f78a85b
Johnnylkc <momozzz1231@gmail.com>
2016-01-09 15:43:38 +0800

    Initial Commit

---
+++ UISlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UISlider
+//
+//  Created by 劉坤昶 on 2016/1/9.
+//  Copyright © 2016年 劉坤昶 Johnny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
